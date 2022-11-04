1767ed2dbe7dbfd353641c07c74777b6b2535505
Ernando Braga <nandobraga78@gmail.com>
2022-11-04 17:55:19 -0300

    atualizando o nome das variaves conforme o projeto

--- src/aplicacaoBar/DesafioBar.java
+++ src/aplicacaoBar/DesafioBar.java
@@ -3,35 +3,35 @@ package aplicacaoBar;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entitiesBar.EntitiesBar;
+import entitiesBar.Biil;
 
 public class DesafioBar {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		EntitiesBar bar = new EntitiesBar();
+		Biil bar = new Biil();
 
 		System.out.print("Sexo: ");
 		bar.gender = (Character.toUpperCase(sc.next().charAt(0)));
-		
+
 		// loop de repetição para válidação do sexo.
 		while (bar.gender != 'M' && bar.gender != 'F') {
 			System.out.print("Sexo inválido! Favor digite um sexo válido: ");
 			bar.gender = (Character.toUpperCase(sc.next().charAt(0)));
 		}
-		
-		//scopu de atribição de quatitativos de consumo
+
+		// scopu de atribição de quatitativos de consumo
 		System.out.print("Quantidade de cervejas: ");
 		bar.beer = sc.nextInt();
 
 		System.out.print("Quantidades de refrigerantes: ");
-		bar.soda = sc.nextInt();
+		bar.softDrink = sc.nextInt();
 
 		System.out.print("Quantidades de espetinhos: ");
 		bar.barbecue = sc.nextInt();
-		
-		//condição do pagamento total.
+
+		// condição do pagamento total.
 		System.out.println("RELATÓTRIO:");
 		if (bar.feeding() < 30.00) {
 			System.out.printf("Consumo = R$ %.2f \n", bar.feeding());
--- src/entitiesBar/Biil.java
+++ src/entitiesBar/Biil.java
@@ -1,17 +1,18 @@
 package entitiesBar;
 
-public class EntitiesBar {
+public class Biil {
 	public char gender;
 	public int beer;
-	public int soda;
 	public int barbecue;
+	public int softDrink;
+	
 
 	public double cover() {
 		return 4.00;
 	}
 
 	public double feeding() {
-		return (beer * 5.00) + (soda * 3.00) + (barbecue * 7.00);
+		return (beer * 5.00) + (softDrink * 3.00) + (barbecue * 7.00);
 	}
 
 	public double ticket() {
